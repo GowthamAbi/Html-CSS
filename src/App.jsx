c6d4f324d79d082239fe723cdbf75cb5e088a323
“GowthamAbi” <gowtham2131ece@gmail.com>
2025-09-25 22:13:49 +0530

    tailwindcss update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './App.css'
 export default function App() {
   return (
-    <div>App</div>
+    <div>
+      <h1 className='text-rose-600'>
+        App
+      </h1>
+      </div>
   )
 }
